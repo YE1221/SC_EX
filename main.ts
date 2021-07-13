5dada3083a8a08ba7fd810b0614525f0c5dde9c2
YE1221 <87366399+YE1221@users.noreply.github.com>
2021-07-13 19:42:24 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,13 +1,18 @@
 input.onButtonPressed(Button.A, function () {
     사람.change(LedSpriteProperty.X, -1)
+    music.playTone(784, music.beat(BeatFraction.Sixteenth))
 })
 input.onButtonPressed(Button.AB, function () {
     control.reset()
 })
 input.onButtonPressed(Button.B, function () {
     사람.change(LedSpriteProperty.X, 1)
+    music.playTone(784, music.beat(BeatFraction.Sixteenth))
 })
 let 사람: game.LedSprite = null
+for (let index = 0; index < 2; index++) {
+    music.playMelody("C D E F G A B C5 ", 255)
+}
 basic.showString("APPLEGAME")
 사람 = game.createSprite(2, 4)
 let 사과 = game.createSprite(randint(0, 4), 0)
@@ -20,12 +25,26 @@ basic.forever(function () {
         }
         if (사과.isTouching(사람)) {
             game.addScore(1)
+            music.playTone(392, music.beat(BeatFraction.Eighth))
+            music.playTone(587, music.beat(BeatFraction.Eighth))
             사과.delete()
             사과 = game.createSprite(randint(0, 4), 0)
         } else {
             game.removeLife(1)
+            music.playTone(523, music.beat(BeatFraction.Eighth))
+            music.playTone(349, music.beat(BeatFraction.Eighth))
             사과.delete()
             사과 = game.createSprite(randint(0, 4), 0)
+        }
+    }
+    for (let index = 0; index < 0; index++) {
+        if (game.isGameOver()) {
+            music.playMelody("C5 B A G F E D C ", 255)
+            music.playMelody("C D E F G A B C5 ", 255)
+            music.playTone(415, music.beat(BeatFraction.Double))
+            music.playTone(392, music.beat(BeatFraction.Double))
+            music.playTone(370, music.beat(BeatFraction.Double))
+            music.playTone(349, music.beat(BeatFraction.Breve))
         }
     }
 })
@@ -37,12 +56,26 @@ basic.forever(function () {
         }
         if (사과.isTouching(사람)) {
             game.addScore(1)
+            music.playTone(392, music.beat(BeatFraction.Eighth))
+            music.playTone(587, music.beat(BeatFraction.Eighth))
             사과.delete()
             사과 = game.createSprite(randint(0, 4), 0)
         } else {
             game.removeLife(1)
+            music.playTone(523, music.beat(BeatFraction.Eighth))
+            music.playTone(349, music.beat(BeatFraction.Eighth))
             사과.delete()
             사과 = game.createSprite(randint(0, 4), 0)
+        }
+    }
+    for (let index = 0; index < 1; index++) {
+        if (game.isGameOver()) {
+            music.playMelody("C5 B A G F E D C ", 259)
+            music.playMelody("C D E F G A B C5 ", 255)
+            music.playTone(415, music.beat(BeatFraction.Double))
+            music.playTone(392, music.beat(BeatFraction.Double))
+            music.playTone(370, music.beat(BeatFraction.Double))
+            music.playTone(349, music.beat(BeatFraction.Breve))
         }
     }
 })
@@ -54,12 +87,26 @@ basic.forever(function () {
         }
         if (사과.isTouching(사람)) {
             game.addScore(1)
+            music.playTone(392, music.beat(BeatFraction.Eighth))
+            music.playTone(587, music.beat(BeatFraction.Eighth))
             사과.delete()
             사과 = game.createSprite(randint(0, 4), 0)
         } else {
             game.removeLife(1)
+            music.playTone(523, music.beat(BeatFraction.Eighth))
+            music.playTone(349, music.beat(BeatFraction.Eighth))
             사과.delete()
             사과 = game.createSprite(randint(0, 4), 0)
+        }
+    }
+    for (let index = 0; index < 1; index++) {
+        if (game.isGameOver()) {
+            music.playMelody("C5 B A G F E D C ", 255)
+            music.playMelody("C D E F G A B C5 ", 255)
+            music.playTone(415, music.beat(BeatFraction.Double))
+            music.playTone(392, music.beat(BeatFraction.Double))
+            music.playTone(370, music.beat(BeatFraction.Double))
+            music.playTone(349, music.beat(BeatFraction.Breve))
         }
     }
 })
@@ -71,13 +118,26 @@ basic.forever(function () {
         }
         if (사과.isTouching(사람)) {
             game.addScore(1)
+            music.playTone(392, music.beat(BeatFraction.Eighth))
+            music.playTone(587, music.beat(BeatFraction.Eighth))
             사과.delete()
             사과 = game.createSprite(randint(0, 4), 0)
         } else {
             game.removeLife(1)
+            music.playTone(523, music.beat(BeatFraction.Eighth))
+            music.playTone(349, music.beat(BeatFraction.Eighth))
             사과.delete()
-            basic.showString("" + (game.score()))
             사과 = game.createSprite(randint(0, 4), 0)
+        }
+    }
+    for (let index = 0; index < 1; index++) {
+        if (game.isGameOver()) {
+            music.playMelody("C5 B A G F E D C ", 255)
+            music.playMelody("C D E F G A B C5 ", 255)
+            music.playTone(415, music.beat(BeatFraction.Double))
+            music.playTone(392, music.beat(BeatFraction.Double))
+            music.playTone(370, music.beat(BeatFraction.Double))
+            music.playTone(349, music.beat(BeatFraction.Breve))
         }
     }
 })
